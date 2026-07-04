66ec25f8da5d7d69c1fa89cd6c412ae13a7b3d39
yudsuzu <yud.suzu@gmail.com>
2026-07-04 21:01:06 +0900

    Update update_gateway.py

--- update_gateway.py
+++ update_gateway.py
@@ -61,7 +61,7 @@ blocklists: List[Dict[str, str]] = [
         "name": "Hagezi Pro++",
         "url": "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus-onlydomains.txt",
         "backup_url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus-onlydomains.txt",
-        "priority": 10000
+        "priority": 5000
     }
 ]
 
